a48007404f3bf22819e90c9fbabf69ca3457f62c
Arg-Jess <79282848+Arg-Jess@users.noreply.github.com>
2022-06-26 18:58:24 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,21 @@
+
+import './styles.css';
+
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
+// import { Todo } from './classes/todo-class';
+// import { TodoList } from './classes/todo-list.class';
+
+export const todoList = new TodoList();
+
+// const tarea = new Todo('Aprender javascript!!');
+// todoList.nuevoTodo( tarea );
+
+// console.log( todoList );
+
+// crearTodoHtml( tarea );
+
+// localStorage.setItem('mi-key', 'ABC123');
+// sessionStorage.setItem('mi-key', 'ABC123');
+
+todoList.todos.forEach( todo => crearTodoHtml ( todo ));
